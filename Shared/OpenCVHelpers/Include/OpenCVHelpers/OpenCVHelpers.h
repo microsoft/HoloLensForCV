cb49cc1895c41e45dd2c9a857de2399d5c4d3344
Pawel Olszta (MSFT) <polszta@microsoft.com>
2018-06-13 22:01:40 -0700

    Add a helper method to wrap a VLC camera image (grayscale packed as 32bpp BGRA) as a grayscale cv::Mat.

--- Shared/OpenCVHelpers/Include/OpenCVHelpers/OpenCVHelpers.h
+++ Shared/OpenCVHelpers/Include/OpenCVHelpers/OpenCVHelpers.h
@@ -16,4 +16,14 @@ namespace rmcv
     void WrapHoloLensSensorFrameWithCvMat(
         _In_ HoloLensForCV::SensorFrame^ holoLensSensorFrame,
         _Out_ cv::Mat& openCVImage);
+
+    /// <summary>
+    /// Wraps a HoloLens Visible Light Camera frame with a cv::Mat. The VLC images
+    /// are 8bpp grayscale, but we deliver them through Media APIs as 32bpp BGRA
+    /// images, with each of the BGRA values representing 4 consecutive grayscale
+    /// pixel intensities.
+    /// </summary>
+    void WrapHoloLensVisibleLightCameraFrameWithCvMat(
+        _In_ HoloLensForCV::SensorFrame^ holoLensSensorFrame,
+        _Out_ cv::Mat& wrappedImage);
 }
